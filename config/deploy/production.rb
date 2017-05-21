536ab13108e3f3b7e7e1d1f61dab4e0e9712aa21
Makoto Kitamura <today.is.sailingday24@gmail.com>
2017-05-22 06:25:35 +0900

    modify files for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,13 @@
+# 環境名
+set :stage, :production
+
+# RAILS_ENVに設定する値
+set :rails_env, :production
+
+role :app, %w{ app@test.mktktmr.net }
+role :web, %w{ app@test.mktktmr.net }
+role :db,  %w{ app@test.mktktmr.net }
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
