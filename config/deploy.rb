d93a11189937502404c698aff699c92ffe37c54f
Makoto Kitamura <today.is.sailingday24@gmail.com>
2017-05-22 05:16:37 +0900

    add confing for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,35 @@
-# config valid only for current version of Capistrano
-lock "3.8.1"
+# config/deploy.rb
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# capistranoのバージョン固定
+lock '3.8.1'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# デプロイするアプリケーション名
+set :application, ENV['APP_NAME']
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# cloneするgitのリポジトリ
+set :repo_url, ENV['REPO_URL']
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# deployするブランチ。デフォルトはmaster
+# この記載をすることで bundle exec cap production deploy BRANCH=ブランチ名で特定のブランチをデプロイできるようになる
+set :branch, ENV['BRANCH'] || 'master'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/' + ENV['APP_NAME']
 
-# Default value for :pty is false
-# set :pty, true
+# シンボリックリンクをはるファイル。(※後述)
+set :linked_files, %w(config/secrets.yml config/database.yml .env)
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+# シンボリックリンクをはるフォルダ。(※後述)
+set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets)
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# 保持するバージョンの個数(※後述)
+set :keep_releases, 5
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# rubyのバージョン
+set :rbenv_ruby, '2.4.1'
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# システムワイドにインストールされているRubyを使用する
+set :rbenv_type, :system
+
+# デプロイ実行時に出力するログのレベル。
+set :log_level, :debug
