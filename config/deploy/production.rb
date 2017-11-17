ae73929083555776c3007f479b0a596822c19f18
ryozo <tobiuokun@gmail.com>
2017-11-18 07:35:29 +0900

    Fix domain_name again

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,6 @@ set :stage, :production
 # RAILS_ENVに設定する値
 set :rails_env, :production
 
-role :app, %w{ app@ryozo-infra.xyz }
-role :web, %w{ app@ryozo-infra.xyz }
-role :db,  %w{ app@ryozo-infra.xyz }
+role :app, %w{ app@app1.ryozo-infra.xyz }
+role :web, %w{ app@app1.ryozo-infra.xyz }
+role :db,  %w{ app@app1.ryozo-infra.xyz }
